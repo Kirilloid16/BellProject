f778d673d0e28557ad46013e4b7bf6646aa51a12
Kirilloid16 <toro-200@mail.ru>
2021-11-18 22:35:06 +0300

    1

---
+++ src/main/java/ru/lukah/bellproject/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ru.lukah.bellproject;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BellProjectApplication.class);
+    }
+
+}
