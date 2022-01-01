64316e137561d804eb09af5a0c8d4fdb61187f8a
Kirilloid16 <toro-200@mail.ru>
2022-01-01 22:16:28 +0300

    1

--- src/main/java/ru/lukah/bellproject/Controllers/restController.java
+++ src/main/java/ru/lukah/bellproject/Controllers/restController.java
@@ -1,19 +1,63 @@
 package ru.lukah.bellproject.Controllers;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class restController {
-    @GetMapping("/showOrg")
+    @GetMapping("/show/organizations")
     public String showOrg(Model model){
         return "showOrg";
     }
 
-    @GetMapping("/{id}")
-    public  String showOrgId(){
+    @GetMapping("/show/organization/{id}")
+    public  String showOrgId(@PathVariable("id")int id){
         return null;
     }
-    
+
+    @PatchMapping("update/organization/{id}/update")
+    public String updateOrgId(@PathVariable("id")int id){
+        return null;
+    }
+    @PostMapping("/organization/post")
+    public String PostOrganization(){
+            return null;
+    }
+
+    @GetMapping("/show/office")
+    public String showOffice(Model model){
+        return "showOffice";
+    }
+
+    @GetMapping("/show/office/{id}")
+    public  String showOfficeId(@PathVariable("id")int id){
+        return null;
+    }
+
+    @PatchMapping("update/office/{id}/update")
+    public String updateOfficeId(@PathVariable("id")int id){
+        return null;
+    }
+    @PostMapping("/office/post")
+    public String PostOffice(){
+        return null;
+    }
+    @GetMapping("/show/user")
+    public String showUser(Model model){
+        return "showUser";
+    }
+
+    @GetMapping("/show/user/{id}")
+    public  String showUserId(@PathVariable("id")int id){
+        return null;
+    }
+
+    @PatchMapping("update/user/{id}/update")
+    public String updateUserId(@PathVariable("id")int id){
+        return null;
+    }
+    @PostMapping("/user/post")
+    public String PostUser(){
+        return null;
+    }
 }
